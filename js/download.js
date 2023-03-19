8e0a77842e59b763c7ecb9bc648ba3816c2cd466
Nikita35356 <106546813+Nikita35356@users.noreply.github.com>
2023-03-19 09:30:58 +0500

    Update download.js

--- js/download.js
+++ js/download.js
@@ -13,46 +13,46 @@ var version = document.location.search.slice(9)
 
 var code = `<h2>В ${version} добавили:</h2>`
 
-if (version != "") {
-    ver()
-}else{
+//if (version != "") {
+//    ver()
+//}else{
     loadVer()
-}
+//}
 
 
 
 
-function ver(){
+// function ver(){
     
-    version = "0.0.3"
+//     version = "0.0.3"
     
-    Style.href = "css/version.css"
-    parse(infoVersion[version],t=true)
-}
+//     Style.href = "css/version.css"
+//     parse(infoVersion[version],t=true)
+// }
 
-function parse(item,t=false) {
+// function parse(item,t=false) {
     
-    for (var i = 0; i < item.length; i++) {
-        code += "<ul>"
+//     for (var i = 0; i < item.length; i++) {
+//         code += "<ul>"
         
-        console.log(item[i]);
-        if (typeof(item[i])=="string") {
-            code += `<li>${item[i]}</li>`
-        } else if (typeof(item[i])=="object") {
-            code += `<li>${item[i][0]}</li>`
-            parse(item[i][1])
-        }
-        code += "</ul>"
+//         console.log(item[i]);
+//         if (typeof(item[i])=="string") {
+//             code += `<li>${item[i]}</li>`
+//         } else if (typeof(item[i])=="object") {
+//             code += `<li>${item[i][0]}</li>`
+//             parse(item[i][1])
+//         }
+//         code += "</ul>"
         
         
-    }
-    if (t) {
+//     }
+//     if (t) {
          
-        console.log(code);
-        main.innerHTML = code
-    }
+//         console.log(code);
+//         main.innerHTML = code
+//     }
     
-}
+// }
 
 
 function loadVer(){
