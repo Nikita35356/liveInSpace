1f33d734fe2e8bd09e46247bfe74a28e08c5e16f
Nikita35356 <106546813+Nikita35356@users.noreply.github.com>
2023-04-06 15:45:55 +0500

    Create modsWiki.js

---
+++ js/modsWiki.js
@@ -0,0 +1,56 @@
+var main = document.getElementsByTagName("main")[0]
+
+var infoVersion = {
+    "0.0.2":["Система строительства(Переделано, на данный момент не доступно для игрока)","Железная руда","Статистика при наведении на строение","Лог"],
+    "0.0.3":[["Золото",["Спавн","Предмет “Золотая руда”"]],["Инвентарь",["Работает кнопка “Выкинуть всё” и “Съесть”(Пока что нет предмета который можно съесть))"]],"Интерфейс (немного изменился)"],
+    "0.0.4":[["Главное меню",["Кнопка играть","Кнопка выход"]],"Система модов"],
+    "0.0.5":["Теперь можно скачивать моды!(их не много))"],
+    "0.0.6":["В разработке)"]
+}
+
+
+
+// if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     Style.href = "css/versionPC.css"
+// }
+
+var version = document.location.search.slice(1).split("=")
+
+main.innerHTML = `<h2>Раздел "${version[1]}"</h2>`
+
+if (version != "") {
+    ver()
+}else{
+    loadVer()
+}
+
+
+
+
+function ver(){
+    //parse(infoVersion[version],t=true)
+}
+
+function parse(item,t=false) {
+    
+    for (var i = 0; i < item.length; i++) {
+        code += "<ul>"
+        
+        console.log(item[i]);
+        if (typeof(item[i])=="string") {
+            code += `<li>${item[i]}</li>`
+        } else if (typeof(item[i])=="object") {
+            code += `<li>${item[i][0]}</li>`
+            parse(item[i][1])
+        }
+        code += "</ul>"
+        
+        
+    }
+    if (t) {
+         
+        console.log(code);
+        main.innerHTML = code
+    }
+    
+}
